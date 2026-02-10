c9fd5a95db734aa94041abc7ce00837488cd45e6
Vaibhav <vaibhav.joswal@gammaedge.io>
2026-02-10 12:33:49 +0530

    Initial FastAPI app with CI pipeline

---
+++ main.py
@@ -0,0 +1,101 @@
+from fastapi import FastAPI, HTTPException, Request
+from pydantic import BaseModel
+import json
+from datetime import datetime
+import os
+
+app = FastAPI()
+
+student_id = 0
+Student = {}
+
+class User(BaseModel):
+    name: str
+    age: int
+
+LOG_FILE = "request_logs.json"
+
+
+@app.middleware("http")
+async def store_request_data(request: Request, call_next):
+    data = {
+        "time": datetime.utcnow().isoformat(),
+        "method": request.method,
+        "path": request.url.path,
+        "headers": dict(request.headers),
+        "cookies": request.cookies,
+        "ip":request.client.host
+    }
+
+    if os.path.exists(LOG_FILE):
+        try:
+            with open(LOG_FILE, "r") as f:
+                logs = json.load(f)
+        except json.JSONDecodeError:
+            logs = []
+    else:
+        logs = []
+
+    logs.append(data)
+
+    
+    with open(LOG_FILE, "w") as f:
+        json.dump(logs, f, indent=2)
+
+    response = await call_next(request)
+    return response
+
+
+@app.get("/")
+def home():
+    return {"message": "Hello"}
+
+
+@app.post("/user")
+def add_user(user: User):
+    global student_id
+
+    item = {
+        "name": user.name,
+        "age": user.age
+    }
+
+    Student[student_id] = item
+    student_id += 1
+
+    with open("data.json", "w") as f:
+        json.dump(Student, f, indent=2)
+
+    return {
+        "message": "SUCCESSFULLY ADDED",
+        "data": item
+    }
+
+
+@app.get("/user")
+def show():
+    
+    if not os.path.exists("data.json"):
+        return {"DATA": {}}
+
+    with open("data.json", "r") as f:
+        data = json.load(f)
+
+    return {"DATA": data}
+
+
+@app.get("/user/{userid}")
+def show_one(userid: int):
+    
+    if not os.path.exists("data.json"):
+        raise HTTPException(status_code=404, detail="User not found")
+
+    with open("data.json", "r") as f:
+        p = json.load(f)
+
+    userid = str(userid)
+
+    if userid in p:
+        return {"data": p[userid]}
+    else:
+        raise HTTPException(status_code=404, detail="User not found")
---
+++ test_app.py
@@ -0,0 +1,4 @@
+from main import app
+
+def test_app_loads():
+    assert app is not None
